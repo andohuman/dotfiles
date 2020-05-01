acc279509ccfef8c44b1704ba614aa188c9255b8
Gautham Venkataraman <gauthsvenkat99@gmail.com>
2020-05-01 15:58:48 +0530

    fixed dumb bugs

--- arch_install/1_install_arch.sh
+++ arch_install/1_install_arch.sh
@@ -2,7 +2,7 @@
 
 parted --script /dev/sda \
 	mklabel gpt \
-	mkpart primary fat32 1MiB 513 MiB \
+	mkpart primary fat32 1MiB 513MiB \
 	set 1 boot on \
 	mkpart primary linux-swap 513MiB 2.5GiB \
 	mkpart primary ext4 2.5GiB 100%
@@ -22,7 +22,7 @@ mount /dev/sda1 /mnt/boot
 
 #download reflector and run it to get the fastest mirrors
 pacman -Sy reflector --noconfirm
-reflector --verbose -l 20gen --sort rate --save /etc/pacman.d/mirrorlist
+reflector --verbose -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 
 #bootstrap the required packages
 pacstrap /mnt base base-devel linux linux-firmware vim networkmanager
@@ -33,6 +33,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 #change root into the fresh installation to configure stuff
 wget https://raw.githubusercontent.com/andohuman/dotfiles/master/arch_install/2_configure_chroot.sh
 mv 2_configure_chroot.sh /mnt
+arch_chroot /mnt chmod +x 2_configure_chroot.sh
 arch-chroot /mnt ./2_configure_chroot.sh
 rm /mnt/2_configure_chroot.sh
 
