5ee9350c8068098899d6df3c2079ab20ac3c7292
Gautham Venkataraman <gauthsvenkat99@gmail.com>
2020-05-01 16:10:00 +0530

    gotta install grub before using it dumbo

--- arch_install/2_configure_chroot.sh
+++ arch_install/2_configure_chroot.sh
@@ -16,6 +16,7 @@ echo "testmachine" >> /etc/hostname
 systemctl enable NetworkManager
 
 #install and configure grub
+pacman -Sy grub efibootmgr --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
